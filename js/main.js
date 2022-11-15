1a4a6c2af22d1cbdaf25fe454a3000d2d60739ba
Taylor Proch <tay_thedev@outlook.com>
2022-11-15 14:19:27 -0500

    Added a better layout for more than one type and ability

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function getPokemon() {
     let choice = document.querySelector('input').value.toLowerCase();
     let url = `https://pokeapi.co/api/v2/pokemon/${choice}`;
 
-    fetch(url)
+    fetch(url) // fetching from Pokeapi.co
 
         .then(res => res.json())
         .then(data => {
@@ -14,11 +14,23 @@ function getPokemon() {
             document.querySelector('h2').innerText = data.name;
             document.querySelector('img').src = data.sprites.front_default;
             document.querySelector('#pokemonType1').innerText = data.types[0].type.name;
-            document.querySelector('#pokemonType2').innerText = data.types[1].type.name;
+
+            if (document.querySelector('#pokemonType2').innerText = data.types[1].type.name) {
+                document.querySelector('#pokemonType2').innerText = data.types[1].type.name;
+                document.querySelector('.span1').style.visibility = 'visible';
+            }
+
+            document.querySelector('#pokemonAbilities').innerText = data.abilities[0].ability.name;
+
+            if (document.querySelector('#pokemonHiddenAbil').innerText = data.abilities[1].ability.name) {
+                document.querySelector('#pokemonHiddenAbil').innerText = data.abilities[1].ability.name;
+                document.querySelector('.span2').style.visibility = 'visible';
+            }
+            
             document.querySelector('#pokemonHeight').innerText = data.height;
             document.querySelector('#pokemonWeight').innerText = data.weight;
-
-        })
+        
+        }) 
         .then(err => {
 
             console.log(`error ${err}`);
